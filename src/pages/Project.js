9f25379db7715a6e0b0643d80666c9a92603fc8f
sunguh0904 <sunguh0904@gmail.com>
2024-07-24 18:59:59 +0900

    #14 feat 프로젝트 페이지 아이템 박스 생성

--- src/pages/Project.js
+++ src/pages/Project.js
@@ -3,10 +3,19 @@ import '../css/projcet.css';
 const Projcet = () => {
     return (
         <>
-            <div className="projcet-wrap">
-                <div className='projcet-header'>
-                    <div className='projcet-header-current-page'>PROJECT</div>
-                    <div className='projcet-header-name'>SUNG U HYEON</div>
+            <div className="project-wrap">
+                <div className='project-header'>
+                    <div className='project-header-current-page'>PROJECT</div>
+                    <div className='project-header-name'>SUNG U HYEON</div>
+                </div>
+                <div className='project-content'>
+                    <div className='project-list'>
+                        <div className='project-item'>
+                            <div className='project-item-title'>Team Project:</div>
+                            <div className='project-item-description'>Panda Office ERP System</div>
+                        </div>
+                    </div>
+                    <div className='project-view'></div>
                 </div>
             </div>
         </>
