654483b71a3a4406b9f2034acd575c016574a155
sunguh0904 <sunguh0904@gmail.com>
2024-07-24 17:10:12 +0900

    #14 feat 프로젝트 페이지 화면 분활 및 나누기 구현

---
+++ src/pages/Project.js
@@ -0,0 +1,11 @@
+import '../css/projcet.css';
+
+const Projcet = () => {
+    return (
+        <>
+            <div className="projcet-wrap"></div>
+        </>
+    )
+}
+
+export default Projcet;
