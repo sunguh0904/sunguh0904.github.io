b091b850e04c22cbb08741d5a8bc92af564e3ffd
sunguh0904 <sunguh0904@gmail.com>
2024-07-24 17:15:09 +0900

    #14 feat 프로젝트 페이지 헤더 구현

--- src/pages/Project.js
+++ src/pages/Project.js
@@ -3,7 +3,12 @@ import '../css/projcet.css';
 const Projcet = () => {
     return (
         <>
-            <div className="projcet-wrap"></div>
+            <div className="projcet-wrap">
+                <div className='projcet-header'>
+                    <div className='projcet-header-current-page'>PROJECT</div>
+                    <div className='projcet-header-name'>SUNG U HYEON</div>
+                </div>
+            </div>
         </>
     )
 }
