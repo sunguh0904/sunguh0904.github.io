9ebcedb4d6c36b76eda04bb1e40e33629d6839a4
sunguh0904 <sunguh0904@gmail.com>
2024-08-07 11:22:00 +0900

    #32 feat 플러그인 설치 및 css-in-js 활용

--- src/page/Main.js
+++ src/page/Main.js
@@ -1,6 +1,16 @@
+import styled from "styled-components";
+
 const Main = () => {
+
+    const H1 = styled.div `
+        font-size: ${props => props.fontSize || '5rem'};
+        color: ${props => props.color || 'red'};
+    `
+    
     return (
-        <></>
+        <>
+            <H1>hello, world!</H1>
+        </>
     )
 }
 
